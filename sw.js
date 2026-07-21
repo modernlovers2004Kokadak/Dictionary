1b185ebfe924efebf221ec31c944b879f60457f2
modernlovers2004Kokadak <modernlovers2004@gmail.com>
2026-07-21 18:59:03 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const CACHE_PREFIX='riyoshi-glossary-';
-const CACHE=CACHE_PREFIX+'v3.0.54-20260721';
-const ASSETS=['./','./index.html','./infection-classification-guide.html','./taisaku-youten.html','./style.css?v=3.0.54','./manifest.webmanifest','./glossaryData.js?v=3.0.54','./glossaryQuizData.js?v=3.0.54','./glossaryApp.js?v=3.0.54','./apple-touch-icon.png','./icon-192.png','./icon-512.png','./favicon-32.png'];
+const CACHE=CACHE_PREFIX+'v3.0.67-20260721-stage4';
+const ASSETS=['./','./index.html','./infection-classification-guide.html','./taisaku-youten.html','./style.css?v=3.0.67','./manifest.webmanifest','./glossaryData.js?v=3.0.67','./glossaryQuizData.js?v=3.0.67','./glossaryApp.js?v=3.0.67','./apple-touch-icon.png','./icon-192.png','./icon-512.png','./favicon-32.png'];
 self.addEventListener('install',event=>{
   event.waitUntil((async()=>{
     const cache=await caches.open(CACHE);
